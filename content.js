0538b69af9fdfef8d5dfad66f2818d46699e11ea
DiscordDelegate <85379840+DiscordDelegate@users.noreply.github.com>
2025-01-25 18:34:18 -0800

    V0.1

---
+++ content.js
@@ -0,0 +1,11 @@
+// Check if Ember is available on the page
+if (window.Ember && window.Ember.getOwner) {
+    const owner = window.Ember.getOwner(document.querySelector('[data-ember-owner]'));
+    if (owner && owner.lookup) {
+      const store = owner.lookup('service:store');
+      if (store) {
+        // Log Ember data to the console
+        console.log('Ember Data:', store.peekAll());
+      }
+    }
+  }
